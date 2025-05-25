9aec7eaa8965ffc794a98173f4a4a480b9f85a5c
oliver <121308232+olivery4@users.noreply.github.com>
2025-05-24 21:21:25 -0600

    Update CoreUnloader.java

--- src/main/content/blocks/distribution/CoreUnloader.java
+++ src/main/content/blocks/distribution/CoreUnloader.java
@@ -10,25 +10,5 @@ public class CoreUnloader extends Unloader {
 
     public CoreUnloader(String name) {
         super(name);
-
-        // General block properties
-        size = 1;
-        health = 150;
-        itemCapacity = 50;
-
-        // Resource requirements to build
-        requirements(Category.distribution, ItemStack.with(
-            Items.titanium, 40,
-            Items.silicon, 25,
-        ));
-
-        // Power usage
-        consumesPower = true;
-        consumePower(50f / 60f); // ~40 power/sec
-
-        buildVisibility = BuildVisibility.shown;
-
-        // Research tree dependency
-        research = "unloader";
     }
 }
