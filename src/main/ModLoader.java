174883dd2dd7607904969f083910487c8fcda9ea
oliver <121308232+olivery4@users.noreply.github.com>
2025-05-24 20:54:53 -0600

    Update ModLoader.java

--- src/main/ModLoader.java
+++ src/main/ModLoader.java
@@ -1,23 +1,14 @@
 package main;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
+import mindustry.mod.Mod;
+import unload.ModBlocks;
 
-public class ModLoader extends Mod{
+public class ModLoader extends Mod {
 
-    public ModLoader(){
-        
-    }
+    public ModLoader() {}
 
     @Override
-    public void loadContent(){
+    public void loadContent() {
         RemoteCoreUnloader.load();
-        Log.info("Loading some example content.");
     }
-
 }
